518003a39d3cf025192e74b3e16b132c5a380ebc
Ram Khatal <rk8796835865@gmail.com>
2022-05-07 10:07:51 +0530

    frist one

---
+++ ClassA.java
@@ -0,0 +1,28 @@
+package Pkg1;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
+		int d = a-b;
+		System.out.println(d);
+		int e = a*b;
+		System.out.println(e);
+		int f = b/a;
+		System.out.println(f);
+		int g =20%10;
+		System.out.println(g);
+		a= a+1;
+		System.out.println(++a);
+		a = a-1;
+		System.out.println(--a);
+		
+				
+		
+
+	}
+
+}
---
+++ ClassB.java
@@ -0,0 +1,33 @@
+package Pkg1;
+
+public class ClassB {
+
+	public static void main(String[] args) {
+	    int a=100;
+	    int b=200;
+	    if(a==100 && b==200)
+	    {
+	    	System.out.println("this is valid");
+	    }
+	    else
+	    {
+	    	System.out.println("this is not valide");
+	    }
+	    if(a==50 || b==150)
+	    {
+	    	System.out.println(50);
+	    }
+	    else {
+	    	System.out.println(150);
+	    }
+	    if(a==100) {
+	    	System.out.println("true");
+	    }
+	    else
+	    {
+	    	System.out.println("false");
+	    }
+
+	}
+
+}
